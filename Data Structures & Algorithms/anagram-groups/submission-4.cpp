98e7c5bc4cee6516f1dee8bbd9299cc01b61d28a
farahweaam <142122374+farahweaam@users.noreply.github.com>
2026-05-29 03:43:38 +0300

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.cpp
@@ -0,0 +1,51 @@
+
+using namespace std;
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> words;
+        if (strs.size() == 0)
+        {
+            return words;
+        }
+        
+        for (string word : strs)
+        {
+            string temp = word;
+            sort(temp.begin(), temp.end());
+            if(words.size()==0)
+            {
+                    vector<string> thisv;
+                    thisv.push_back(word);
+                    words.push_back(thisv);
+                    continue;
+            }
+            int wordss = words.size();
+            for (int i = 0;i < wordss;i++)
+            {
+                
+                string wordtemp = words[i][0];
+                
+                //cout<<wordtemp<<endl;
+                sort(wordtemp.begin(), wordtemp.end());
+                if (temp == wordtemp)
+                {
+                    //cout<<word<<" "<<words[i][0]<<endl;
+                    words[i].push_back(word);
+                    break;
+                }
+                else {
+                    if(i==words.size()-1)
+                    {
+                        vector<string> thisv;
+                        thisv.push_back(word);
+                        words.push_back(thisv);
+                    }
+                    
+                }
+            }
+
+        }
+        return words;
+    }
+};
